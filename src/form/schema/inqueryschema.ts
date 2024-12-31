6cab820b137f3c7d13f34c2b79723181146eb9a8
gaurav <gaurav@centocode.com>
2024-12-31 15:25:22 +0530

    chnages in enquery form

--- src/form/schema/inqueryschema.ts
+++ src/form/schema/inqueryschema.ts
@@ -35,16 +35,9 @@ class StudentForm {
   @CreateDateColumn()
   submittedAt: Date;
 
-  //foreginkey
-  // @ManyToOne(() => Form, (form) => form.studentForms, { nullable: false, onDelete: 'CASCADE' })
-  // @JoinColumn({ name: 'formEnqueryId' })
-  // form: Form;
+  @Column()
 
-  // @Column()
-  // enformUuid: string;
-
-  // @Column({type: 'varchar', length: 255, nullable: false})
-  // formEnqueryId: string;
+  customId: string;
 }
 
 export default StudentForm;
