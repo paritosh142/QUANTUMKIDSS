4e02807f0dbef5d1eb54c6954d017964ca259e8a
gaurav <gaurav@centocode.com>
2025-01-10 11:23:41 +0530

    Fix token issue in  reset password

--- src/admin/dto/reset-password.dto.ts
+++ src/admin/dto/reset-password.dto.ts
@@ -1,11 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class ResetPasswordDto {
+  @ApiProperty({ description: 'The current password', format: 'password' })
+  @IsString()
+  @IsNotEmpty()
+  currentPassword: string;
+
+  @ApiProperty({ description: 'The reset token' })
+  @IsString()
+  @IsOptional()
+  token: string;
+
   @ApiProperty({ description: 'The reset token' })
   @IsString()
   @IsNotEmpty()
-  token: string;
+  username: string;
 
   @ApiProperty({ description: 'The new password', format: 'password' })
   @IsString()
