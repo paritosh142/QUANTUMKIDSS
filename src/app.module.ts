6fdb63be876c7786aeb2330ab3b130632e6af877
Paritosh Indalia <paritosh1332@gmail.com>
2025-01-13 14:41:01 +0530

    Add Twilio dependency and update database connection details

--- src/app.module.ts
+++ src/app.module.ts
@@ -31,10 +31,10 @@ import { FeeReceiptGenerate } from './fee-receipt-generate/entities/fee-receipt-
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '132.148.179.19',
+      host: '148.72.247.10',
       port: 3306, 
       username: 'developersqk_2025',
-      password: 'quantumkids@2025',
+      password: 'QuantumKids@2025!',
       database: 'quantumkids',
       entities: [Form,FeeReceiptSchema,,FeeReceiptGenerate,FeeManagement,StudentForm,AddTeam,Admin,Blog,NewsEvent,Picture, __dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
@@ -52,4 +52,4 @@ import { FeeReceiptGenerate } from './fee-receipt-generate/entities/fee-receipt-
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {} 
